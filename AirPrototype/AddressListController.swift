28e8693d3195dda0f1a6f7029b2cbb6b8fc205e9
mtasota <miketasota@gmail.com>
2015-07-13 11:54:41 -0400

    Initial commit

---
+++ AirPrototype/AddressListController.swift
@@ -0,0 +1,25 @@
+//
+//  AddressListController.swift
+//  AirPrototype
+//
+//  Created by mtasota on 7/13/15.
+//  Copyright (c) 2015 CMU Create Lab. All rights reserved.
+//
+
+import UIKit
+
+class AddressListController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
