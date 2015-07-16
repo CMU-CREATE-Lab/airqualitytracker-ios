7327fe1f905fc125d99cde046726696e02d632d5
mtasota <miketasota@gmail.com>
2015-07-16 10:32:17 -0400

    Some tests with MKLocalSearch and CLGeocoder.

--- AirPrototype/ResultsControllerAddressSearch.swift
+++ AirPrototype/ResultsControllerAddressSearch.swift
@@ -8,10 +8,50 @@
 
 import Foundation
 import UIKit
+//import MapKit
+//
+//class ResultsControllerAddressSearch: UITableViewController, UISearchResultsUpdating {
+//    
+//    let handler: MKLocalSearchCompletionHandler = {(response: MKLocalSearchResponse!, error: NSError!) in
+//        if error == nil {
+//            for mapItem in response.mapItems {
+//                NSLog("Name: \(mapItem.name)")
+//            }
+//        } else {
+//            NSLog("error is not nil: \(error.localizedDescription)")
+//        }
+//    }
+//    
+//    
+//    func updateSearchResultsForSearchController(searchController: UISearchController) {
+//        let text = searchController.searchBar.text
+//        NSLog("updateSearchResultsForSearchController with text " + text)
+//        var searchRequest = MKLocalSearchRequest.alloc()
+//        searchRequest.naturalLanguageQuery = text
+//        var localSearch = MKLocalSearch(request: searchRequest)
+//        localSearch.startWithCompletionHandler(handler)
+//    }
+//    
+//}
+
+import CoreLocation
 
 class ResultsControllerAddressSearch: UITableViewController, UISearchResultsUpdating {
     
+    let handler: CLGeocodeCompletionHandler = {(response: [AnyObject]!, error: NSError!) in
+        if error == nil {
+            for placemark in response {
+                let temp = placemark as! CLPlacemark
+                NSLog("got placemark \(temp.name)")
+            }
+        }
+    }
+    
     func updateSearchResultsForSearchController(searchController: UISearchController) {
-        NSLog("updateSearchResultsForSearchController")
+        let text = searchController.searchBar.text
+        NSLog("updateSearchResultsForSearchController with text " + text)
+        
+        CLGeocoder().geocodeAddressString(text, completionHandler: handler)
     }
 }
+
