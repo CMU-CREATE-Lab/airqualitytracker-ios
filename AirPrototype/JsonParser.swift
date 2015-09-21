b9886278c39364df87bae5bd620585faf50e6993
mtasota <miketasota@gmail.com>
2015-09-21 16:42:32 -0400

    Changes for JSON Parser to work without crashing.

--- AirPrototype/JsonParser.swift
+++ AirPrototype/JsonParser.swift
@@ -59,38 +59,46 @@ class JsonParser {
         let result = Feed()
         var feedChannels = Array<Channel>()
         
+        // TODO is it possible for any other ones to be null?
         let feed_id = dataEntry.valueForKey("id") as! Int
         let name = dataEntry.valueForKey("name") as! String
         let exposure = dataEntry.valueForKey("exposure") as! String
         let isMobile = dataEntry.valueForKey("isMobile") as! Int
-        let latitude = dataEntry.valueForKey("latitude") as! Double
-        let longitude = dataEntry.valueForKey("longitude") as! Double
+        let latitude = dataEntry.valueForKey("latitude") as? Double
+        let longitude = dataEntry.valueForKey("longitude") as? Double
         let productId = dataEntry.valueForKey("productId") as! Int
         
         result.feed_id = feed_id
         result.name = name
         result.exposure = exposure
         result.isMobile = isMobile == 1
-        result.location.latitude = latitude
-        result.location.longitude = longitude
+        if latitude != nil && longitude != nil {
+            result.location.latitude = latitude!
+            result.location.longitude = longitude!
+        } else {
+            result.location.latitude = 0
+            result.location.longitude = 0
+        }
         result.productId = productId
         
-        let channels = dataEntry.valueForKey("channelBounds")?.valueForKey("channels") as! NSDictionary
-        let keys = channels.keyEnumerator()
-        while let channelName = keys.nextObject() as? String {
-            // Only grab channels that we care about
-            if let index = Constants.channelNames.indexOf(channelName) {
-                // NOTICE: we must also make sure that this specific channel was updated
-                // in the past 24 hours ("maxTime").
-                let channel = channels.valueForKey(channelName) as! NSDictionary
-                let channelTime = channel.valueForKey("maxTimeSecs") as! Double
-                if channelTime >= maxTime {
-                    feedChannels.append(JsonParser.parseChannelFromJson(channelName, feed: result, dataEntry: channel))
-                    break
+        if let channels = dataEntry.valueForKey("channelBounds")?.valueForKey("channels") as? NSDictionary {
+            let keys = channels.keyEnumerator()
+            while let channelName = keys.nextObject() as? String {
+                // Only grab channels that we care about
+                if let index = Constants.channelNames.indexOf(channelName) {
+                    // NOTICE: we must also make sure that this specific channel was updated
+                    // in the past 24 hours ("maxTime").
+                    let channel = channels.valueForKey(channelName) as! NSDictionary
+                    let channelTime = channel.valueForKey("maxTimeSecs") as! Double
+                    if channelTime >= maxTime {
+                        feedChannels.append(JsonParser.parseChannelFromJson(channelName, feed: result, dataEntry: channel))
+                        break
+                    }
                 }
             }
+            result.channels = feedChannels
         }
-        result.channels = feedChannels
+        
         return result
     }
     
