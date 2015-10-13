5c51f13f29bec8bb3b9baf82a8ad94d9feaf980f
mtasota <miketasota@gmail.com>
2015-10-13 13:50:03 -0400

    Finally got a scroll view working for the about AQI page.

---
+++ AirPrototype/AboutAirQualityController.swift
@@ -0,0 +1,14 @@
+//
+//  AboutAirQualityController.swift
+//  AirPrototype
+//
+//  Created by mtasota on 10/6/15.
+//  Copyright © 2015 CMU Create Lab. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AboutAirQualityController:UIViewController {
+    
+}
