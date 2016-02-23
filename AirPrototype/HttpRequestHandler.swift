ac27bcceb31fb4df4d252a3237a23b41783dee38
mtasota <miketasota@gmail.com>
2016-02-23 14:12:27 -0500

    nowcast: refactored HttpRequestHandler

--- AirPrototype/HttpRequestHandler.swift
+++ AirPrototype/HttpRequestHandler.swift
@@ -19,38 +19,37 @@ class HttpRequestHandler {
     }
     
     
-    func sendRequest(urlRequest: NSMutableURLRequest, completionHandler: ((NSURL?, NSURLResponse?, NSError?) -> Void) ) {
+    private func send(urlRequest: NSMutableURLRequest, additionalHeaders: [NSObject:AnyObject]?, completionHandler: ((NSURL?, NSURLResponse?, NSError?) -> Void) ) {
         let sessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        sessionConfiguration.HTTPAdditionalHeaders = additionalHeaders
         let session = NSURLSession(configuration: sessionConfiguration)
         let downloadTask = session.downloadTaskWithRequest(urlRequest, completionHandler: completionHandler)
         downloadTask.resume()
+    }
+    
+    
+    func sendRequest(urlRequest: NSMutableURLRequest, completionHandler: ((NSURL?, NSURLResponse?, NSError?) -> Void) ) {
+        self.send(urlRequest, additionalHeaders: nil, completionHandler: completionHandler)
     }
     
     
     func sendJsonRequest(urlRequest: NSMutableURLRequest, completionHandler: ((NSURL?, NSURLResponse?, NSError?) -> Void) ) {
-        let sessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
-        sessionConfiguration.HTTPAdditionalHeaders = [
+        let additionalHeaders = [
             "Content-Type" : "application/json"
         ]
-        let session = NSURLSession(configuration: sessionConfiguration)
-        let downloadTask = session.downloadTaskWithRequest(urlRequest, completionHandler: completionHandler)
-        downloadTask.resume()
+        self.send(urlRequest, additionalHeaders: additionalHeaders, completionHandler: completionHandler)
     }
 
 
     func sendAuthorizedJsonRequest(authToken: String, urlRequest: NSMutableURLRequest, completionHandler: ((NSURL?, NSURLResponse?, NSError?) -> Void) ) {
-        let sessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
-        sessionConfiguration.HTTPAdditionalHeaders = [
+        let additionalHeaders = [
             "Content-Type" : "application/json",
             "Authorization" : "Bearer \(authToken)"
         ]
-        let session = NSURLSession(configuration: sessionConfiguration)
-        let downloadTask = session.downloadTaskWithRequest(urlRequest, completionHandler: completionHandler)
-        downloadTask.resume()
+        self.send(urlRequest, additionalHeaders: additionalHeaders, completionHandler: completionHandler)
     }
     
     
-    // TODO consider CLGeocoder: func geocodeAddressString(String, CLGeocodeCompletionHandler)
     func requestGeocodingFromApi(input: String, completionHandler: ((NSURL?, NSURLResponse?, NSError?) -> Void)) {
         let request = HttpHelper.generateRequest("http://autocomplete.wunderground.com/aq?query=\(input)&c=US", httpMethod: "GET")
         GlobalHandler.sharedInstance.httpRequestHandler.sendJsonRequest(request, completionHandler: completionHandler)
