a06e4bdf1fc072a58ec61079be3190461c53bc7f
mtasota <miketasota@gmail.com>
2016-05-16 15:06:19 -0400

    dirty-days: Set up new controller with segue.

---
+++ AirPrototype/DailyTrackerController.swift
@@ -0,0 +1,20 @@
+//
+//  DailyTrackerController.swift
+//  AirPrototype
+//
+//  Created by Mike Tasota on 5/16/16.
+//  Copyright © 2016 CMU Create Lab. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DailyTrackerController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSLog("DailyTrackerController did load")
+    }
+    
+}
