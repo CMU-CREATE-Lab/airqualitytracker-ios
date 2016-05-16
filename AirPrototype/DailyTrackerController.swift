1f5dd0baaffb1c40543741c635832a9943fc8879
mtasota <miketasota@gmail.com>
2016-05-16 15:55:26 -0400

    dirty-days: Added controller ui elements with picker.

--- AirPrototype/DailyTrackerController.swift
+++ AirPrototype/DailyTrackerController.swift
@@ -9,12 +9,45 @@
 import Foundation
 import UIKit
 
-class DailyTrackerController: UIViewController {
+class DailyTrackerController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var webView: UIWebView!
+    var pickerValues = ["Mean", "Median", "Max"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         NSLog("DailyTrackerController did load")
+        
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        
+        // TODO webview height-width ratio should be 1:5
+        // TODO load HTML with javascript
+    }
+    
+    
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    
+    // The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerValues.count
+    }
+    
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerValues[row]
+    }
+    
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        NSLog("Did select row \(pickerValues[row])")
     }
     
 }
