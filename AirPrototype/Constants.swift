563936a3a744cb930868ec3cb42dd216a11d7d47
mtasota <miketasota@gmail.com>
2016-05-18 16:59:39 -0400

    version change (2.5)

--- AirPrototype/Constants.swift
+++ AirPrototype/Constants.swift
@@ -13,7 +13,7 @@ let MATH_PI = 3.14159265358979
 
 class Constants {
     
-    static let APP_VERSION_NUMBER = "2.4"
+    static let APP_VERSION_NUMBER = "2.5"
     
     static let USES_BACKGROUND_SERVICES = true
     
